5dddb55bc5727fa7e4434f6bcd497c95ed106b63
Vitali Haravy <vharavy@fb.com>
2023-10-26 20:57:50 +0100

    Increase tapping term to 300ms

--- keyboards/crkbd/keymaps/vharavy/config.h
+++ keyboards/crkbd/keymaps/vharavy/config.h
@@ -27,7 +27,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #ifdef TAPPING_TERM
 #undef TAPPING_TERM
 #endif
-#define TAPPING_TERM 200
+#define TAPPING_TERM 300
 
 // Enable rapid switch from tap to hold, disables double tap hold auto-repeat.
 #ifdef QUICK_TAP_TERM
